d072c1f028d9b6e92d667d195b70aff995c1ee22
Wizard684 <78129370+Wizard684@users.noreply.github.com>
2021-09-18 16:33:52 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,5 +4,5 @@ from pyrogram import Client, Filters
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
     helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url
-    And Please Join For Watch Hindi Tv Serial @HindiTvFlix"
+    And Please Join For Watch Hindi Tv Serial  @HindiTvFlix"
     await message.reply_text(helptxt)
